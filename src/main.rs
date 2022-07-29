ec54f74117dcfb19d1fe1e46440cb2dc41fff6c2
alexandergw <alexgw@gmail.com>
2022-07-29 19:18:34 +0100

    Initial Windows API testing

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,64 @@
 use std::env;
 use std::process::Command;
 use windows::{
-    core::*, Data::Xml::Dom::*, Win32::Foundation::*, Win32::System::Threading::*,
-    Win32::UI::WindowsAndMessaging::*,
+    core::*, UI::Notifications::*,
+    Data::Xml::Dom::*
 };
 
 fn main() {
-    // println!("Hello, world!");
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
+    // Windows notification
     let output = if cfg!(target_os = "windows") {
-        // Command::new("notify-send")
-        //     .arg("foo bar")
-        //     .output()
-        //     .expect("");
+
+        // Get a toast XML template
+        let toast_xml = ToastNotificationManager::GetTemplateContent(
+            ToastTemplateType::ToastText02
+        ).unwrap();
+
+        // Fill in the text elements
+        let toast_text_elements = toast_xml.GetElementsByTagName(
+            &HSTRING::new()
+        ).unwrap(); // text
         
-    } else {
+        toast_text_elements
+            .Item(0).unwrap()
+            .AppendChild(&toast_xml
+                .CreateTextNode(&HSTRING::from("Hello from Rust!")).unwrap()
+                .cast::<IXmlNode>().unwrap()
+            ).unwrap();
+        
+        toast_text_elements
+            .Item(1).unwrap()
+            .AppendChild(&toast_xml
+                .CreateTextNode(&HSTRING::from("This is some more text.")).unwrap()
+                .cast::<IXmlNode>().unwrap()
+            ).unwrap();
+
+        // Create the toast and attach event listeners
+        let toast = ToastNotification::CreateToastNotification(&toast_xml).unwrap();
+
+        // Show the toast. Use PowerShell's App ID to circumvent the need to register one (this is only an example!).
+        let result =
+            ToastNotificationManager::CreateToastNotifierWithId(
+                &HSTRING::from("{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\\WindowsPowerShell\\v1.0\\powershell.exe")
+            ).unwrap()
+            .Show(&toast).unwrap();
+        
+        dbg!(result);
+    }
+
+    else if cfg!(target_os = "macos") {
+
+    }
+    
+    // Linux
+    else {
+
+        // KDE?
+
+        // Gnome
         Command::new("notify-send")
             .arg("foo bar")
             .output()
